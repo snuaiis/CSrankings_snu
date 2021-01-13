6b8553a5ca958edb54c1b3f0a88beaec5c1ecf56
sjho <sjh2177@gmail.com>
2021-01-13 16:13:56 +0900

    Add 10 faculties and check duplicated faculties.

--- master_to_upload.py
+++ master_to_upload.py
@@ -4,34 +4,47 @@ import pandas as pd
 import requests
 import io
 
-# path of csrankings.csv
-master_path = "master.csv"
-upload_path = "upload.csv"
+# Path of csrankings.csv
+master_path = 'master.csv'
+upload_path = 'upload.csv'
+school = 'Seoul National University'
 
-# csrankings file update from github
+# Csrankings file update from github
 r = requests.get('https://raw.githubusercontent.com/emeryberger/CSrankings/gh-pages/csrankings.csv')
 
+# Load two csv files each
 upload = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
-#upload = master[master["affiliation"] == "Seoul National University"]
 master = pd.read_csv(master_path)
 
-faculty_names = master["name"].tolist()
-master = master[list(upload)]
+# Sort master.csv by korean name and save
+master = master.sort_values(by='korean_name')
+master.to_csv(master_path, mode='w', index=False)
 
-for i, name in enumerate(faculty_names) :
-    faculty = master[master["name"]==name]
-    facluty_original = upload[upload["name"]==name]
-    if facluty_original.empty :
+master[master.duplicated(subset=['scholarid'], keep=False)].to_csv('duplicated.csv')
+
+faculty_id = master['scholarid'].tolist() # Faculty scholarid in master.csv
+master = master[list(upload)] # Leave columns in upload.csv only
+
+# Add or replace faculty rows
+for i, id in enumerate(faculty_id) :
+    faculty = master[master['scholarid']==id] # Faculty row in master.csv
+    facluty_original = upload[upload['scholarid']==id] # Faculty row in upload.csv
+
+    # If faculty not exists in upload.csv, add this in upload.csv
+    if facluty_original.empty : 
         upload = upload.append(faculty)
-        print(str(i+1) + " : " + name + " is appended successfully.")
+        name = faculty.iloc[0]['name']
+        print(str(i+1) + ' : ' + name + ' is appended successfully.')
+    # If not, check for duplicates in two rows - 'faculty', 'facluty_original'.
     else :
         facluty_original = facluty_original.append(faculty)
+        # If not duplicated, replace 'facluty_original' to 'faculty' in upload.csv.
         if not sum(facluty_original.duplicated()) :
-            upload = upload[upload["name"]!=name]
+            upload = upload[upload['scholarid']!=id]
             upload = upload.append(faculty)
-            print(str(i+1) + " : " + name + " is editted successfully.")
+            name = faculty.iloc[0]['name']
+            print(str(i+1) + ' : ' + name + ' is editted successfully.')
         
-
+# Sort upload.csv by name and save
 upload = upload.sort_values(by='name')
-
-upload.to_csv(upload_path, mode = 'w', index=False)
+upload.to_csv(upload_path, mode='w', index=False)
